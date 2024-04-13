4f3132ff9975498bf3d1102cbb416eee7d8e026a
SurfboardV2ray <125461738+Surfboardv2ray@users.noreply.github.com>
2024-04-13 05:09:14 -0400

    Update sorter.py

--- python/sorter.py
+++ python/sorter.py
@@ -26,7 +26,11 @@ def country_code_to_emoji(country_code):
 
 def set_remarks_from_custom_url(url, custom_url_base, counter):
     if url.startswith('vmess://'):
-        decoded_str = base64.b64decode(url[8:].encode('utf-8', 'ignore')).decode('utf-8')
+        try:
+            decoded_str = base64.b64decode(url[8:].encode('utf-8', 'ignore')).decode('utf-8')
+        except UnicodeDecodeError:
+            print(f"Invalid base64 or UTF-8 string: {url[8:]}")
+            return None, None
         config = json.loads(decoded_str)
         host = config['add']
     else:
